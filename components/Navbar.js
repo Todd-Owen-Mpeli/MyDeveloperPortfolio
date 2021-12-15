44efedf769774e35d8e1edb4d4fb87d5ca6d4e08
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-12-15 14:20:35 +0000

    Created new scss variables chaned/edited the navigation scss styling.

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,66 +7,64 @@ const Navbar = () => {
 		<>
 			{/* <!--===== HEADER =====--> */}
 			<header className={styles.header}>
-				<nav>
-					<div className="container">
-						<div className={styles.content}>
-							<div className={styles.navTitle}>
-								<Link href="/">
-									<a>
-										<Image
-											src="/otherImages/LogoYellow.png"
-											alt="Logo"
-											width={30}
-											height={30}
-											objectFit="contain"
-										></Image>
-									</a>
-								</Link>
-								<Link href="/">
-									<a>
-										<Image
-											src="/otherImages/logoNew.png"
-											alt="New Project Logo"
-											width={50}
-											height={50}
-											objectFit="contain"
-										></Image>
-									</a>
-								</Link>
-							</div>
-							<ul className={styles.navLinks}>
-								<Link href="/" target="blank">
-									<a>Projects</a>
-								</Link>
-								<Link href="/" target="blank">
-									<a>About</a>
-								</Link>
-								<Link href="/" target="blank">
-									<a>Skills</a>
-								</Link>
-								<Link href="/" target="blank">
-									<a>Contact</a>
-								</Link>
-								<newButton>
-									<Link href="/">
-										<a
-											className={styles.resumeBtn}
-											target="blank"
-											href="/Todd Owen Mpeli - Web Developer CV.pdf"
-											download="Todd Owen Mpeli CV.pdf"
-										>
-											<Image
-												src="/otherImages/LogoResume.png"
-												alt="New Project Logo"
-												width={50}
-												height={50}
-												objectFit="contain"
-											></Image>
-										</a>
-									</Link>
-								</newButton>
-							</ul>
+				<nav className="container">
+					<div className={styles.content}>
+						<div className={styles.navTitle}>
+							<Link href="/">
+								<a>
+									<Image
+										src="/otherImages/LogoYellow.png"
+										alt="Logo"
+										width={20}
+										height={20}
+										objectFit="contain"
+									></Image>
+								</a>
+							</Link>
+							<Link href="/">
+								<a>
+									<Image
+										src="/otherImages/logoNew.png"
+										alt="New Project Logo"
+										width={35}
+										height={35}
+										objectFit="contain"
+									></Image>
+								</a>
+							</Link>
 						</div>
+						<ul className={styles.navLinks}>
+							<Link href="/" target="blank">
+								<a>Projects</a>
+							</Link>
+							<Link href="/" target="blank">
+								<a>About</a>
+							</Link>
+							<Link href="/" target="blank">
+								<a>Skills</a>
+							</Link>
+							<Link href="/" target="blank">
+								<a>Contact</a>
+							</Link>
+							<newButton>
+								<Link href="/">
+									<a
+										className={styles.resumeBtn}
+										target="blank"
+										href="/Todd Owen Mpeli - Web Developer CV.pdf"
+										download="Todd Owen Mpeli CV.pdf"
+									>
+										<Image
+											src="/otherImages/LogoResume.png"
+											alt="New Project Logo"
+											width={35}
+											height={35}
+											objectFit="contain"
+										></Image>
+									</a>
+								</Link>
+							</newButton>
+						</ul>
 					</div>
 				</nav>
 			</header>
