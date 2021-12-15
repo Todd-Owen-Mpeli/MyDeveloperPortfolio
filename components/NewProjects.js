27ee0cc5f22a0dff4ed64cac2980cbe07c96b782
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-12-15 16:54:02 +0000

    Added content in to the newProjects.js file.

--- components/NewProjects.js
+++ components/NewProjects.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "/styles/Home.module.scss";
 
 const NewProjects = () => {
@@ -7,7 +8,58 @@ const NewProjects = () => {
 			{/* <!--===== NEXT JS PROJECTS =====--> */}
 			<div className={styles.nextJSProjects}>
 				<div className="container">
-					<div className={styles.content}></div>
+					<div className={styles.content}>
+						<div className={styles.leftSide}>
+							<Link href="/">
+								<a>
+									<Image
+										src="/img/Size-Chart-LP_Mens.webp"
+										alt="Product Image"
+										width={300}
+										height={300}
+										layout="responsive"
+										objectFit="cover"
+									></Image>
+								</a>
+							</Link>
+							<div className={styles.title}>
+								<h2>Our Options</h2>
+								<h5>
+									Choose Between our Individual service option. Or our more
+									Business tailored dry cleaning service.
+								</h5>
+							</div>
+						</div>
+						<div className={styles.RightSide}>
+							<Link href="/">
+								<a>
+									<Image
+										src="/img/Size-Chart-LP_Mens.webp"
+										alt="Product Image"
+										width={300}
+										height={300}
+										layout="responsive"
+										objectFit="cover"
+									></Image>
+								</a>
+							</Link>
+							<div className={styles.title}>
+								<Link href="/">
+									<a>
+										<h2>Our Options</h2>
+									</a>
+								</Link>
+								<Link href="/">
+									<a>
+										<h5>
+											Choose Between our Individual service option. Or our more
+											Business tailored dry cleaning service.
+										</h5>
+									</a>
+								</Link>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
