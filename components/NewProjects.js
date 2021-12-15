4e267ffdca4952f37611d05e96218092797f35cc
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-12-15 14:50:40 +0000

    Created a newProjects.js component file for the new next js projects I created. Also created a scss styling section for the new component.

---
+++ components/NewProjects.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from "/styles/Home.module.scss";
+
+const NewProjects = () => {
+	return (
+		<>
+			{/* <!--===== NEXT JS PROJECTS =====--> */}
+			<div className={styles.nextJSProjects}>
+				<div className="container">
+					<div className={styles.content}></div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default NewProjects;
