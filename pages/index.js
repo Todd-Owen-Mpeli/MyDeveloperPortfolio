89267a3b8e5c7367c559873e7a6b01ddcad27c04
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-12-15 13:55:13 +0000

    Updated the body styling fo rthe entire website. Edited the navigation section in the navbar.js and edited the index.js.

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
 			<Navbar />
 
 			{/* <!--===== HERO =====--> */}
-			{/* <Hero /> */}
+			<Hero />
 
 			{/* <!--===== PROJECTS =====--> */}
 			{/* <Projects /> */}
