84c10cde5b11006cb704eb8b0abfe1a44af7194a
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-12-16 12:12:38 +0000

    Edited the footer content and the footer scss styling.

--- components/Footer.js
+++ components/Footer.js
@@ -3,85 +3,42 @@ import styles from "/styles/Home.module.scss";
 
 const Footer = () => {
 	return (
-		<div>
+		<>
 			{/* <!--===== FOOTER =====--> */}
 			<footer className={styles.footer}>
 				<div className={styles.content}>
 					<div className={styles.topSection}>
 						<div className={styles.leftSection}>
 							<div className={styles.additionalInformation}>
-								<Link href="#" target="blank">
-									<a>Gift Cards</a>
+								<Link href="/">
+									<a>Projects</a>
 								</Link>
-								<Link href="#" target="blank">
-									<a>Sign Up For Email</a>
+								<Link href="/">
+									<a>Github Portfolio</a>
 								</Link>
-								<Link href="#" target="blank">
-									<a>Become A Member</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Student Discount</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Feedback</a>
-								</Link>
-							</div>
-							<div className={styles.getHelp}>
-								<Link href="#" target="blank">
-									<a className={styles.titleSection}>GET HELP</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Order Status</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Shipping and Delivery</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Returns</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Payments Options</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Contact Us</a>
-								</Link>
-							</div>
-							<div className={styles.aboutToddBlue}>
-								<Link href="#" target="blank">
-									<a className={styles.titleSection}>ABOUT TODDBLUE</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>News</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Careers</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Investors</a>
-								</Link>
-								<Link href="#" target="blank">
-									<a>Sustainability</a>
+								<Link href="/">
+									<a>Skills</a>
 								</Link>
 							</div>
 						</div>
 						<div className={styles.rightSection}>
-							<Link href="#" target="blank">
-								<a>
+							<Link href="/">
+								<a target="blank">
 									<i className="bi bi-instagram"></i>
 								</a>
 							</Link>
-							<Link href="#" target="blank">
-								<a>
+							<Link href="/">
+								<a target="blank">
 									<i className="bi bi-twitter"></i>
 								</a>
 							</Link>
-							<Link href="#" target="blank">
-								<a>
+							<Link href="/">
+								<a target="blank">
 									<i className="bi bi-youtube"></i>
 								</a>
 							</Link>
-							<Link href="#" target="blank">
-								<a>
+							<Link href="/">
+								<a target="blank">
 									<i className="bi bi-facebook"></i>
 								</a>
 							</Link>
@@ -89,47 +46,30 @@ const Footer = () => {
 					</div>
 					<div className={styles.borderLineMid}></div>
 					<div className={styles.bottomSection}>
-						<div className={styles.topHalfSection}>
-							<Link href="#Guides" target="blank">
-								<a>Guides</a>
-							</Link>
-							<Link href="#TermsOfUse" target="blank">
-								<a>Terms Of Use</a>
-							</Link>
-							<Link href="#TermsOfSale" target="blank">
-								<a>Terms Of Sale</a>
-							</Link>
-							<Link href="#CompanyDetails" target="blank">
-								<a>Company Details</a>
-							</Link>
-							<Link href="#UKModernSlaveryActDisclosure" target="blank">
-								<a>UK Modern Slavery Act Disclosure</a>
-							</Link>
-						</div>
 						<div className={styles.bottomHalfSection}>
 							<div className={styles.leftSide}>
-								<Link href="#location" target="blank">
+								<Link href="/">
 									<a className={styles.location}>
 										<i className="bi bi-geo-alt-fill"></i>United Kingdom
 									</a>
 								</Link>
-								<Link href="#copyright" target="blank">
-									<a>&copy; 2021 ToddBlue. Inc All Rights Reserved</a>
+								<Link href="/">
+									<a>&copy; 2022 Junior Web Developer Portfolio </a>
 								</Link>
 							</div>
 							<div className={styles.rightSide}>
-								<Link href="#Privacy&CookiePolicy" target="blank">
-									<a>Privacy & Cookie Policy</a>
+								<Link href="/Privacy&CookiePolicy">
+									<a>Email Me</a>
 								</Link>
-								<Link href="#CookieSettings" target="blank">
-									<a>Cookie Settings</a>
+								<Link href="/CookieSettings">
+									<a>Download Resume</a>
 								</Link>
 							</div>
 						</div>
 					</div>
 				</div>
 			</footer>
-		</div>
+		</>
 	);
 };
 
--- components/NewProjects.js
+++ components/NewProjects.js
@@ -9,56 +9,8 @@ const NewProjects = () => {
 			<div className={styles.nextJSProjects}>
 				<div className="container">
 					<div className={styles.content}>
-						<div className={styles.leftSide}>
-							<Link href="/">
-								<a>
-									<Image
-										src="/img/Size-Chart-LP_Mens.webp"
-										alt="Product Image"
-										width={300}
-										height={300}
-										layout="responsive"
-										objectFit="cover"
-									></Image>
-								</a>
-							</Link>
-							<div className={styles.title}>
-								<h2>Our Options</h2>
-								<h5>
-									Choose Between our Individual service option. Or our more
-									Business tailored dry cleaning service.
-								</h5>
-							</div>
-						</div>
-						<div className={styles.RightSide}>
-							<Link href="/">
-								<a>
-									<Image
-										src="/img/Size-Chart-LP_Mens.webp"
-										alt="Product Image"
-										width={300}
-										height={300}
-										layout="responsive"
-										objectFit="cover"
-									></Image>
-								</a>
-							</Link>
-							<div className={styles.title}>
-								<Link href="/">
-									<a>
-										<h2>Our Options</h2>
-									</a>
-								</Link>
-								<Link href="/">
-									<a>
-										<h5>
-											Choose Between our Individual service option. Or our more
-											Business tailored dry cleaning service.
-										</h5>
-									</a>
-								</Link>
-							</div>
-						</div>
+						<div className={styles.leftSide}></div>
+						<div className={styles.RightSide}></div>
 					</div>
 				</div>
 			</div>
